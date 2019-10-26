f11ff45463793acc811fb1a862f790069aa280a2
大大大肥凯 <dafeikai@gmail.com>
2019-10-26 14:41:31 +0800

    整理目录

--- 15-three-sum/index.test.js
+++
@@ -1,8 +0,0 @@
-const { threeSum } = require('./two-pointer')
-const assert = require('assert')
-
-describe(`测试正确性`, () => {
-  it(`题目例子`, () => {
-    assert.deepEqual(threeSum([-2,0,0,2,2]), [[-2, 0, 2]])
-  })
-})
--- 16-three-sum-closest/index.spec.js
+++
@@ -1,9 +0,0 @@
-const assert = require('power-assert')
-const { threeSumClosest } = require('./two-pointer')
-
-describe(`测试双指针法`, _ => {
-  it(`题目例子`, done => {
-    assert( threeSumClosest([1, 2, -1, -4], 1) === 2 )
-    done()
-  })
-})
