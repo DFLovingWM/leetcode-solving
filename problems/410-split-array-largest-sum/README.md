08a8aa0db6f91e2016af921c4b2dd18cd7efde8f
大大大肥凯 <dafeikai@gmail.com>
2019-10-26 22:35:46 +0800

    二分枚举

---
+++ problems/410-split-array-largest-sum/README.md
@@ -0,0 +1,9 @@
+# 410. 分割数组的最大值
+
+## 思路
+
+相当于切蛋糕，使最大块最小（minMax）。二分枚举最大块，然后检验能否
+
+## 相似题目
+
+- [1231. 分享巧克力](https://leetcode-cn.com/problems/divide-chocolate/)，最大化最小值
--- problems/410-split-array-largest-sum/binary-search.js
+++ problems/410-split-array-largest-sum/binary-search.js
@@ -1,37 +1,35 @@
 /**
- * 二分枚举最大和，检验是否存在（@todo 为什么count要从1数起）
+ * 二分枚举最大和（minMax）
  * 时间：O(NlogX)
  */
 var splitArray = function (nums, K) {
-  const sum = nums.reduce((a, b) => a + b, 0)
-  if (sum === Math.pow(2, 31) - 1) return 
+  let max = -Infinity
+  let sum = 0
+  for (const n of nums) {
+    max = Math.max(max, n)
+    sum += n
+  }
 
-  let left = 0
+  let left = max
   let right = sum + 1
 
   while (left < right) {
-    const mid = left + Math.floor((right - left + 1) / 2) // 左中位数
-    const n = cut(nums, mid) // 切成n份
+    const mid = left + Math.floor((right - left) / 2)
 
-    if (n < K) { // 满足，就找更小的
+    if (check(nums, mid, K)) { // 满足，就找更小的
       right = mid
     } else {
       left = mid + 1
-    }
-
-    if (n >= K) { // 份数足够，说明候选值满足条件或较小，所以还能扩大一点
-      left = mid
-    } else { // 份数不够，说明候选值过大，需要缩小
-      right = mid - 1
     }
   }
 
   return left
 };
 
-function cut (nums, maxSum) {
+function check (nums, maxSum, K) {
   let acc = 0
   let count = 0
+
   for (const n of nums) {
     if (acc + n > maxSum) {
       ++count
@@ -39,7 +37,9 @@ function cut (nums, maxSum) {
     }
     acc += n
   }
-  return count
+  if (acc > 0) ++count // 如果有剩余，也当作一份
+
+  return count <= K // 看是否
 }
 
 [
